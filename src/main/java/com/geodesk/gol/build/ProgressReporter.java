14bdc6f3a45f490173863c52fe14da39cba1ba84
deseo <martindesmond+clarisma@gmail.com>
2022-11-01 13:20:08 +0100

    copyright

--- src/main/java/com/geodesk/gol/build/ProgressReporter.java
+++ src/main/java/com/geodesk/gol/build/ProgressReporter.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) Clarisma / GeoDesk contributors
+ *
+ * This source code is licensed under the AGPL 3.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 package com.geodesk.gol.build;
 
 import com.clarisma.common.text.Format;
--- src/main/java/com/geodesk/gol/compiler/STagTable.java
+++ src/main/java/com/geodesk/gol/compiler/STagTable.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) Clarisma / GeoDesk contributors
+ *
+ * This source code is licensed under the AGPL 3.0 license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 package com.geodesk.gol.compiler;
 
 import com.clarisma.common.math.Decimal;

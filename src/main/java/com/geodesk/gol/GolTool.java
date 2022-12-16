b11f829d65cb27b4c772331cb8a6f59215bde6cd
deseo <martindesmond+clarisma@gmail.com>
2022-12-16 16:48:14 +0100

    Release 0.1.3

--- src/main/java/com/geodesk/gol/GolTool.java
+++ src/main/java/com/geodesk/gol/GolTool.java
@@ -16,7 +16,7 @@ import java.io.PrintWriter;
 
 public class GolTool extends Application
 {
-    public static final String VERSION = "0.1.3b";
+    public static final String VERSION = "0.1.3";
 
     @Override public String version()
     {

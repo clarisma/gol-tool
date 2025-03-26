79d193d8dc6bea30368093e018488a928b7b3078
clarisma <56932851+clarisma@users.noreply.github.com>
2025-03-26 16:46:03 +0100

    Release 1.0.1

--- src/main/java/com/geodesk/gol/GolTool.java
+++ src/main/java/com/geodesk/gol/GolTool.java
@@ -12,7 +12,7 @@ import com.clarisma.common.cli.Application;
 
 public class GolTool extends Application
 {
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.0.1";
 
     @Override public String version()
     {
